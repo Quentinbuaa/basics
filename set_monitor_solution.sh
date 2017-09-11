826138a5e1a4900f2a9e206ca6f9556427e9a306
Kun Qiu <qiukun11@sina.com>
2017-09-11 09:13:25 -0700

    Add set monitor solution script

---
+++ set_monitor_solution.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# sudo apt-get install arandr
+xrandr --output Virtual3 --off --output Virtual2 --off --output Virtual1 --mode 1600x900 --pos 0x0 --rotate normal --output Virtual7 --off --output Virtual6 --off --output Virtual5 --off --output Virtual4 --off --output Virtual8 --off
